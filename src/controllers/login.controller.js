54de6083dd6ce4ec6a95ff6493801a1afa4f76cf
a0p0bj7 <a0p0bj7@M-C02F26F2MD6N.local>
2021-11-22 12:10:21 -0300

    [aperez] variable entorno en archivo .env corregida para la BD

--- src/controllers/login.controller.js
+++ src/controllers/login.controller.js
@@ -5,20 +5,31 @@ dotenv.config();
 
 class LoginPlayerController {
     getPlayerById = async (req, res, next) => {
-        const player = await cuentaModel.find(req.body);
 
-        if (!player[0]) {
-            const mensaje = `Usuario ${req.body.email_login} no encontrado`;
-            // throw new HttpError(404, 'Player no encontrado');
-            res.status(404).render('../views/pages/info', { mensaje, isLogin: false });
+        const player = await cuentaModel.find(req.body)
+            .then((player) => {
+                if (!player[0]) {
+                    const mensaje = `Usuario ${req.body.email_login} no encontrado`;
+                    // throw new HttpError(404, 'Player no encontrado');
+                    res.status(404).render('../views/pages/info', { mensaje, isLogin: false });
 
-        }
+                } else {
+                    const { nombre, email } = player[0];
+                    console.log(nombre);
+                    console.log(email);
+                    const mensaje = `Hola ${nombre} ahora puedes reproducir simular el live streaming con Zapping!`;
+                    res.status(200).render('../views/pages/player', { mensaje, isLogin: true });
+                }
 
-        const { nombre, email } = player[0];
-        console.log(nombre);
-        console.log(email);
-        const mensaje = `Hola ${nombre} ahora puedes reproducir simular el live streaming con Zapping!`;
-        res.status(200).render('../views/pages/player', { mensaje, isLogin: true });
+
+            })
+            .catch((e) => {
+                const mensaje = `Algo falló, error: ${e}`;
+                console.log(`mensaje ${mensaje}`);
+                // throw new HttpError(404, 'Player no encontrado');
+                res.status(500).render('../views/pages/info', { mensaje, isLogin: false });
+            });
+
     };
 }
 
